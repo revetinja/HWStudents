5d83becfc55875d7db15c47d293e2df38956310d
revetinja <adis.reve@gmail.com>
2014-06-09 01:35:04 +0200

    Homework Student Class IComparable Email

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HWStudents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int count;
+        static string name = "Adis";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                List<Student> students = new List<Student>(); // List of students
+                students.Add(new Student("asjo1525@aubih.edu.ba", "Adis", "Jugo"));
+                students.Add(new Student("nmkn1527@aubih.edu.ba", "Nedim", "Kulašin"));
+                students.Add(new Student("kmkc1643@aubih.edu.ba", "Kerim", "Krdžević"));
+                students.Add(new Student("eknc1552@aubih.edu.ba", "Eldin", "Kasapović"));
+                students.Add(new Student("uryl1552@aubih.edu.ba", "Umur", "Yuksel"));
+                students.Add(new Student("inhr1555@aubih.edu.ba", "Irfan", "Hujdur"));
+
+                students.Sort(); // student sort
+                count = students.Count(); // counting of students and putting into static int variable
+
+                foreach (Student s in students) // inputing students on listbox
+                {
+                    listBox1.Items.Add(s);
+                }
+
+                label1.Text = "Number of students: " + count; // ispis broja studenata
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWStudents
+{
+    class Student : IComparable<Object>
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        // Constructor
+        public Student(string _email, string _name, string _lastName)
+        {
+            email = _email;
+            name = _name;
+            lastName = _lastName;
+        }
+
+        // This is comparing
+        public int CompareTo(Object obj)
+        {
+            if (obj == null) return 1;
+
+            if (this.email == ((Student)obj).email)
+                return this.email.CompareTo(((Student)obj).email);
+            return ((Student)obj).email.CompareTo(this.email)*(-1); // ascending order
+        }
+
+        //override of function ToString
+        public override string ToString()
+        {
+            return "Email: " + email + ", name: " + name + ", last name: " + lastName;
+        }
+    }
+}
